5cf4d64fcd12a0cfe34a6381449f1ee4d8fd93ab
ehime <dodomeki@gmail.com>
2018-09-26 09:36:05 -0700

    readding for no prompt

--- providers.tf
+++ providers.tf
@@ -1,5 +1,6 @@
 provider "aws" {
   alias                   = "eu-west"
+  region                  = "eu-west-2"
   profile                 = "${var.aws_profile}"
   shared_credentials_file = "${pathexpand("~/.aws/config")}"
   version                 = "~> 1.34"
@@ -7,6 +8,7 @@ provider "aws" {
 
 provider "aws" {
   alias                   = "us-west"
+  region                  = "us-west-2"
   profile                 = "${var.aws_profile}"
   shared_credentials_file = "${pathexpand("~/.aws/config")}"
   version                 = "~> 1.34"
@@ -14,6 +16,7 @@ provider "aws" {
 
 provider "aws" {
   alias                   = "us-east"
+  region                  = "us-east-1"
   profile                 = "${var.aws_profile}"
   shared_credentials_file = "${pathexpand("~/.aws/config")}"
   version                 = "~> 1.34"
@@ -21,6 +24,7 @@ provider "aws" {
 
 provider "aws" {
   alias                   = "ap-south"
+  region                  = "ap-south-1"
   profile                 = "${var.aws_profile}"
   shared_credentials_file = "${pathexpand("~/.aws/config")}"
   version                 = "~> 1.34"
